ed07cab3c7586c8f7bea71b585c01ef6709b39eb
ashfaq9958 <137767028+ashfaq9958@users.noreply.github.com>
2025-05-16 00:27:19 +0530

    cretaed model for Message and user

---
+++ src/model/User.ts
@@ -0,0 +1,89 @@
+// ////////////////////////////////////////////
+
+import mongoose, { Schema, Document } from "mongoose";
+
+// Subdocument interface (no Document inheritance needed)
+interface IMessage {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema = new Schema<IMessage>(
+  {
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  },
+  { _id: false }
+);
+
+// Main document interface
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: IMessage[];
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email"],
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      trim: true,
+    },
+    verifyCode: {
+      type: String,
+      required: [true, "Verify Code is required"],
+    },
+    verifyCodeExpiry: {
+      type: Date,
+      required: [true, "Verify code expiry is required"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAcceptingMessage: {
+      type: Boolean,
+      default: true,
+    },
+    messages: {
+      type: [MessageSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+
+const UserModel =
+  mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
+export default UserModel;
